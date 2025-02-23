a0b51107c32393cf597e43cc09fddd327f773c33
Stropheum <stropheum060@gmail.com>
2025-02-22 20:45:42 -0500

    implemented lazysingleton

--- Assets/Scripts/LazySingleton.cs
+++ Assets/Scripts/LazySingleton.cs
@@ -4,6 +4,29 @@ namespace NISGAB
 {
     public class LazySingleton<T> : MonoBehaviour where T : LazySingleton<T>
     {
-        protected static T _instance;
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var obj = new GameObject("<Singleton> " + typeof(T).Name);
+                    _instance = obj.AddComponent<T>();    
+                }
+                
+                return _instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Debug.LogError("Duplicate Singleton Instance. Destroying...");
+                Destroy(this);
+            }
+        }
     }
 }
