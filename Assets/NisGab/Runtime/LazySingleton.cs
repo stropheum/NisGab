803587a1988b556f3ac18bb6d1acae4732ad664b
Stropheum <stropheum060@gmail.com>
2025-03-27 12:38:43 -0400

    made package UMP compliant

--- Assets/NisGab/Runtime/LazySingleton.cs
+++ Assets/NisGab/Runtime/LazySingleton.cs
@@ -10,8 +10,7 @@ namespace NisGab
     {
         private static T _instance;
 
-        public static T Instance
-        {
+        public static T Instance {
             get
             {
                 if (_instance == null)
